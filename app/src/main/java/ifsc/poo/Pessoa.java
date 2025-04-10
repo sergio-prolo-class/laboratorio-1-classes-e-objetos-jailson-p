c60e197727d1a2bf09953064bc642c0d679bf108
jailson-p <jailson.p@aluno.ifsc.edu.br>
2025-04-10 08:59:12 -0300

    Finalizado classe Pesoa.java

---
+++ app/src/main/java/ifsc/poo/Pessoa.java
@@ -0,0 +1,25 @@
+package ifsc.poo;
+
+public class Pessoa {
+    private String nome;
+    private int idade = 0;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
