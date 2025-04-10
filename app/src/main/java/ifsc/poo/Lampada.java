1d3e7b0882b08786b39c54d8606c988d28c17d8d
jailson-p <jailson.p@aluno.ifsc.edu.br>
2025-04-10 08:29:00 -0300

    Lampada.java feito

---
+++ app/src/main/java/ifsc/poo/Lampada.java
@@ -0,0 +1,25 @@
+package ifsc.poo;
+
+public class Lampada {
+
+    private boolean ligada = true;
+
+
+    public Lampada(){
+        this.ligada = true;
+    }
+    public Lampada(boolean ligada){
+        this.ligada = ligada;
+    }
+
+    public void ligar(){
+        this.ligada = true;
+    }
+    public void desligar(){
+        this.ligada = false;
+    }
+    public boolean verEstado(){
+        return this.ligada;
+    }
+
+}
